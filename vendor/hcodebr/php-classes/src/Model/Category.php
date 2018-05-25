bf0fb9048ea757ecfd656056e12598606a4acf93
Clemilton Pereira <clemilton.ufam@gmail.com>
2018-05-25 19:11:00 -0400

    Admin categorias busca e paginacao

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -155,6 +155,50 @@ class Category extends Model{
 		]);
 	}
 
+	public static function getPage($page =1 ,$itemsPerPage = 10){
+
+		$start=($page-1)*$itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT sql_calc_found_rows * 
+			FROM  tb_categories
+			ORDER BY descategory
+			LIMIT $start,$itemsPerPage;
+
+		");
+
+		$resultsTotal = $sql->select("SELECT found_rows() as nrtotal;");
+
+		return [
+			"data" => $results,
+			"total"=>(int)$resultsTotal[0]["nrtotal"],
+			"pages"=>ceil($resultsTotal[0]["nrtotal"]/$itemsPerPage)
+		];
+	}
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
+		$start = ($page - 1) * $itemsPerPage;
+		$sql = new Sql();
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM  tb_categories
+			WHERE descategory LIKE :search
+			ORDER BY descategory
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+	} 
+
 
 }
 
