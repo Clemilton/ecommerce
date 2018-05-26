29c0aed90961feb77a9b3f1f86c3057f931b521d
Clemilton Pereira <clemilton.ufam@gmail.com>
2018-05-25 22:18:00 -0400

    Ajuste endereco

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -54,13 +54,12 @@ class Address extends Model{
 
 	public function save()
 	{
-
 		$sql = new Sql();
-
-		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
 			':idaddress'=>$this->getidaddress(),
 			':idperson'=>$this->getidperson(),
 			':desaddress'=>utf8_decode($this->getdesaddress()),
+			':desnumber'=>$this->getdesnumber(),
 			':descomplement'=>utf8_decode($this->getdescomplement()),
 			':descity'=>utf8_decode($this->getdescity()),
 			':desstate'=>utf8_decode($this->getdesstate()),
@@ -68,11 +67,9 @@ class Address extends Model{
 			':deszipcode'=>$this->getdeszipcode(),
 			':desdistrict'=>$this->getdesdistrict()
 		]);
-
 		if (count($results) > 0) {
 			$this->setData($results[0]);
 		}
-
 	}
 
 	public static function setMsgError($msg){
